866bead47c053dcf0802c63f1b47b7dbc1734ca2
Unknown <exanimatesi@yandex.ru>
2018-10-26 15:27:19 +0700

    Added decode qr-code

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,7 +3,12 @@
     <div class="mdl-grid">
       <div class="mdl-cell mdl-cell--3-col mdl-cell mdl-cell--1-col-tablet mdl-cell--hide-phone"></div>
       <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-phone">
-        <qrcode-reader></qrcode-reader>
+        <qrcode-reader @decode="onDecode">
+
+        </qrcode-reader>
+        <div v-show="decodeString !== ''">
+          {{decodeString}}
+        </div>
       </div>
     </div>
   </div>
@@ -14,6 +19,16 @@
 
   import { QrcodeReader } from 'vue-qrcode-reader'
   export default {
-    components: { QrcodeReader }
+    data: function () {
+      return {
+        decodeString: ''
+      }
+    },
+    components: { QrcodeReader },
+    methods: {
+      onDecode (decodedString) {
+        this.decodeString = decodedString
+      }
+    }
   }
 </script>
