ea0a1e3c7789d6c68ae0c8c591156246c6e89207
Unknown <exanimatesi@yandex.ru>
2018-10-29 18:55:01 +0700

    Added bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import vueSignature from 'vue-signature'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 Vue.use(vueSignature)
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
