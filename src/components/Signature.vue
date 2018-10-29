ffd20220478804f93146e8d4d263b2136d6095d0
Unknown <exanimatesi@yandex.ru>
2018-10-29 15:53:22 +0700

    Added vue-signature

--- src/components/Signature.vue
+++ src/components/Signature.vue
@@ -1,15 +1,107 @@
 <template>
-  <div>
-    Hello Signature
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mt-2">
+          <vue-signature class="signature" ref="signature" :sigOption="option" :w="'90%'" :h="'400px'"></vue-signature>
+          <img v-bind:src="lastUrl" v-show="lastUrl !== ''"/>
+        </div>
+
+        <div class="button_container">
+          <button class="btn btn-outline-primary float-left" @click="save">Save</button>
+          <button class="btn btn-outline-primary float-left" @click="clear">Clear</button>
+          <button class="btn btn-outline-primary float-left" @click="undo">Undo</button>
+          <button class="btn btn-outline-primary float-left" @click="addWaterMark">addWaterMark</button>
+        </div>
+      </div>
+    </div>
+
+
+
+
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Signature'
+    name: 'Signature',
+    data: function () {
+      return {
+        option: {
+          penColor: 'rgb(0, 0, 0)',
+          backgroundColor: 'rgb(255,255,255)'
+        },
+        lastUrl: ''
+      }
+    },
+    methods: {
+      save () {
+        var _this = this
+        var png = _this.$refs.signature.save()
+        var jpeg = _this.$refs.signature.save('image/jpeg')
+        var svg = _this.$refs.signature.save('image/svg+xml')
+        console.log(png)
+        console.log(jpeg)
+        console.log(svg)
+        _this.lastUrl = png
+      },
+      clear () {
+        var _this = this
+        _this.lastUrl = ''
+        _this.$refs.signature.clear()
+      },
+      undo () {
+        var _this = this
+        _this.$refs.signature.undo()
+      },
+      addWaterMark () {
+        var _this = this
+        _this.$refs.signature.addWaterMark({
+          text: 'mark text', // watermark text, > default ''
+          font: '20px Arial', // mark font, > default '20px sans-serif'
+          style: 'all', // fillText and strokeText,  'all'/'stroke'/'fill', > default 'fill
+          fillStyle: 'red', // fillcolor, > default '#333'
+          strokeStyle: 'blue', // strokecolor, > default '#333'
+          x: 100, // fill positionX, > default 20
+          y: 200, // fill positionY, > default 20
+          sx: 100, // stroke positionX, > default 40
+          sy: 200 // stroke positionY, > default 40
+        })
+      },
+      fromDataURL (url) {
+        var _this = this
+        _this.$refs.signature1.fromDataURL(url)
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .signature {
+    margin-top: 5px;
+    border: double 3px transparent;
+    border-radius: 5px;
+    background-image: linear-gradient(white, white),
+    radial-gradient(circle at top left, #4bc5e8, #9f6274);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+  }
+  /*img {
+    border: double 3px transparent;
+    margin-top: 5px;
+    border-radius: 5px;
+    radial-gradient(circle at top left, #4bc5e8, #9f6274);
+  }*/
+  .button_container{
+    margin-top: 5px;
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .col-12 {
+    margin-right: auto;
+  }
+  .btn {
+    margin-left: 5px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import vueSignature from 'vue-signature'
 
 Vue.config.productionTip = false
+Vue.use(vueSignature)
 
 /* eslint-disable no-new */
 new Vue({
