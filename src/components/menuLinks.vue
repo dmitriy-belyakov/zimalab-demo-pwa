3706748affc117b5ffa0d94ac00ef4055329c6a8
Dima Belyakov <dmitriy.b@zimalab.com>
2018-12-12 12:01:20 +0700

    added sounds on click and fixed width of menu items

--- src/components/menuLinks.vue
+++ src/components/menuLinks.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="menu-links">
         <router-link v-for="link in links" :key="link" :to="link.path" class="mdl-navigation__link" @click.native="hideDrawer">
             {{ link.name }}
         </router-link>
@@ -23,8 +23,9 @@
                 })
             })
         },
-        methods: {
+        methods: {            
             hideDrawer () {
+                this.$parent.playButtonSound()
                 if (this.autoHideDrawer) {
                     document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
                     document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
