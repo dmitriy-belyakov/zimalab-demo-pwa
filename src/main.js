c365f3bf1241f15af9a60852688cf3fc731c1b67
Dima Belyakov <dmitriy.b@zimalab.com>
2018-12-12 21:19:19 +0700

    added vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import vueSignature from 'vue-signature'
 import BootstrapVue from 'bootstrap-vue'
 // import 'bootstrap/dist/css/bootstrap.css'
@@ -11,10 +12,12 @@ import BootstrapVue from 'bootstrap-vue'
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(vueSignature)
+Vue.use(store)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
