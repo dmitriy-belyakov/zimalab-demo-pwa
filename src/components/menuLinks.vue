1e5e659338c8910ca5206b416536a5e1d734fcab
Dima Belyakov <dmitriy.b@zimalab.com>
2018-12-12 11:48:21 +0700

    created menuLinks component with menu links

---
+++ src/components/menuLinks.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <router-link v-for="link in links" :key="link" :to="link.path" class="mdl-navigation__link" @click.native="hideDrawer">
+            {{ link.name }}
+        </router-link>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+    export default {
+        props: ['autoHideDrawer'],
+        data: function () {
+            return {
+                links: []
+            }
+        },
+        mounted () {
+            this.$router.options.routes.forEach(route => {
+                this.links.push({
+                    name: route.name,
+                    path: route.path
+                })
+            })
+        },
+        methods: {
+            hideDrawer () {
+                if (this.autoHideDrawer) {
+                    document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+                    document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+                }
+            }
+        }
+    }
+</script>
