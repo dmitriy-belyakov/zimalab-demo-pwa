b8e95f8d8e8a93cbe38652a708d7a5e13c7bd2e3
Dima Belyakov <dmitriy.b@zimalab.com>
2018-12-27 11:36:09 +0700

    renamed Camera.vue to ImageUpload.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import QRCodeScanner from '@/views/QRCodeScanner'
 import Signature from '@/views/Signature'
 import Geotag from '@/views/Geotag'
-import Camera from '@/views/Camera'
+import ImageUpload from '@/views/ImageUpload'
 import OfflineExample from '@/views/OfflineExample'
 import Workers from '@/views/Workers'
 import Purchase from '@/views/Purchase'
@@ -39,7 +39,7 @@ export default new Router({
     {
       path: '/imageupload',
       name: 'Image upload',
-      component: Camera
+      component: ImageUpload
     },
     {
       path: '/imagecrop',
