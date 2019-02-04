54e67b515406310178609e3a7dd75553f1caf987
Dima Belyakov <dmitriy.b@zimalab.com>
2019-02-04 16:46:06 +0700

    created skeleton for login page (without functionality)

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,40 @@
 <template>
     <div class="container">
-        <h1 class="header">
-            hello
-        </h1>
+        <h3 class="header">
+            Login
+        </h3>
+
+        <b-input v-model="email" type="text" placeholder="Email"/>
+        <b-input v-model="password" type="password" placeholder="Password"/>
+
+        <b-button class="login-button" variant="primary" :disabled="!(email && password)">
+            Login
+        </b-button>
     </div>
 </template>
+
+<script>
+    /* eslint-disable */
+    export default {
+        data: () => {
+            return {
+                email: '',
+                password: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container {
+        max-width: 400px;
+    }
+
+    input {
+        margin-bottom: 10px;
+    }
+
+    .login-button {
+        width: 100%;
+    }
+</style>
